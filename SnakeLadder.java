02dd1947ace73c16ef2b61ca1383d43d1adf83ba
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-02-15 21:24:08 +0530

    Added SnakeLadder UC-2

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package snakeLadder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		int roll = (int) Math.floor((Math.random() * 10) % 6)+1;
+		
+		  System.out.println("player rolls die and gets value =" +roll);
+	}
+}
